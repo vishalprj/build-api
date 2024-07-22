49ce0c082f9eca15e9009531d31279a82ab4fb7d
Vishal Prajapati <85871658+vishalprj@users.noreply.github.com>
2024-07-22 22:01:21 +0530

    Add Pagination in api

--- Controllers/products.js
+++ Controllers/products.js
@@ -17,9 +17,13 @@ const getAllProducts = async (req,res) => {
         let sorting = sort.replace(",", " ");
         apiData = apiData.sort(sorting)
      }
+     let page = Number(req.query.page) || 1;
+     let limit = Number(req.query.limit) || 3;
+     let skip = (page - 1) * limit
+     apiData = apiData.skip(skip).limit(limit)
 
     const data = await apiData
-    res.status(200).json({data})
+    res.status(200).json({data, nbHits:data.length})
 }
 const getAllProductsTest = async (req,res) => {
     const data = await product.find(req.query)
